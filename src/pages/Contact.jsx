0410ebfb94f3f33f3cc793b850fd6986c36ef8c2
Pranish97 <99318459+Pranish97@users.noreply.github.com>
2025-06-16 22:11:11 +0545

    Creating Contact Page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import contactImage from "../public/images/contact.png";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="contact-container">
+      <div className="contact-image-box">
+        <h1>Contact Us</h1>
+      </div>
 
-export default Contact
+      <div className="contact-form-wrapper">
+        <div className="contact-form-box">
+          <h1>Let's Get In Touch.</h1>
+          <p>Or Just Reach Out to quickList@gmail.com</p>
+          <form className="contact-form">
+            <div className="form-row">
+              <div className="form-group">
+                <label>First Name</label>
+                <input type="text" placeholder="Enter your first name..." />
+              </div>
+              <div className="form-group">
+                <label>Last Name</label>
+                <input type="text" placeholder="Enter your last name..." />
+              </div>
+            </div>
+
+            <div className="form-group">
+              <label>Email Address</label>
+              <input type="email" placeholder="Enter your email address..." />
+            </div>
+
+            <div className="form-group">
+              <label>Phone Number</label>
+              <input type="tel" placeholder="+977 98XXXXXXXX" />
+            </div>
+
+            <div className="form-group">
+              <label>Message</label>
+              <textarea
+                placeholder="Enter your main text here..."
+                maxLength="300"
+              />
+            </div>
+
+            <button type="submit" className="submit-btn">
+              Submit Form
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
